e5df93753b26e11c0498a3ac07a320e77545e023
Syde2 <contact@syde2hub.com>
2024-04-04 17:08:09 +0200

    Update d'hier

---
+++ src/components/global/EdgeCard.vue
@@ -0,0 +1,49 @@
+<script setup>
+
+</script>
+
+
+<template>
+
+<div class="box_parent">
+    <div class="box2">
+    </div>
+    <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
+        <defs>
+            <filter id="flt_tag">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
+                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="flt_tag" />
+                <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
+            </filter>
+        </defs>
+    </svg>
+
+</div>
+
+
+
+</template>
+
+<style>
+.box2
+{
+    width: 322px;
+    background-color: #faebd7;
+    height: 229px;
+    clip-path: polygon(100% 100%,0% 100%,0% 92px,100% 0%);
+    margin: 0 auto;
+}
+
+.flt_svg {
+    visibility: hidden;
+    position: absolute;
+    width: 0px;
+    height: 0px;
+}
+
+.box_parent
+{
+    filter: url('#flt_tag');
+}
+
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,10 @@
 <script setup>
+import EdgeCard from 'src/components/global/EdgeCard.vue'
 </script>
 
 <template>
   <q-page class="flex flex-center">
+    <EdgeCard />
   </q-page>
 </template>
 
