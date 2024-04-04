6f6a933bf03c4d42f4cf17bd72e4eb9ef820577b
Syde2 <contact@syde2hub.com>
2024-04-04 21:00:29 +0200

    Ajout props img couleur

--- src/components/global/EdgeCard.vue
+++ src/components/global/EdgeCard.vue
@@ -1,4 +1,19 @@
 <script setup>
+import {ref} from 'vue'
+
+const props = defineProps({
+  imageUrl : {
+    type:String,
+    default:'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=2810&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  label : {
+    type:String,
+    default:'Label'
+  },
+  color :String
+})
+
+const image = `url(${props.imageUrl})`
 
 </script>
 
@@ -7,7 +22,7 @@
 
 <div class="box_parent ">
     <div class="box2 reverse">
-      <h3> Menu </h3>
+      <h3> {{props.label}} </h3>
     </div>
     <div class="box2 background">
 
@@ -27,11 +42,10 @@
 
 </template>
 
-<style>
-.box2
-{
+<style lang="scss">
+.box2{
     width: 322px;
-    background-color: #faebd7;
+    background-color: v-bind(color);
     height: 229px;
     clip-path: polygon(100% 100%,0% 100%,0% 62px,100% 0%);
     margin: 0 auto;
@@ -43,36 +57,20 @@
     align-items: center;
     font-size: 3rem;
 
-
-
 }
 .reverse{
   rotate: 180deg;
-  background-image: linear-gradient(
-  45deg,
-  hsl(34deg 78% 91%) 0%,
-  hsl(40deg 79% 90%) 10%,
-  hsl(45deg 80% 90%) 20%,
-  hsl(49deg 81% 89%) 30%,
-  hsl(52deg 81% 88%) 40%,
-  hsl(55deg 82% 87%) 50%,
-  hsl(55deg 69% 88%) 60%,
-  hsl(54deg 54% 88%) 70%,
-  hsl(54deg 38% 89%) 80%,
-  hsl(53deg 20% 89%) 90%,
-  hsl(0deg 0% 90%) 100%
-);
+
 }
 .reverse h3{
   transform: scale(-1, -1);
-  text-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 
 }
 .background{
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
-  background-image: url('https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=2810&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+  background-image: v-bind(image);
 }
 
 .flt_svg {
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,17 @@
 <script setup>
 import EdgeCard from 'src/components/global/EdgeCard.vue'
+const menuUrl = 'https://images.unsplash.com/photo-1524237492620-14fcdd03ce16?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+const platUrl = 'https://plus.unsplash.com/premium_photo-1675676486598-733979a2218f?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+const ingredientUrl = 'https://plus.unsplash.com/premium_photo-1677619680770-4feb47c4ebcc?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 </script>
 
 <template>
-  <q-page class="flex flex-center">
-    <EdgeCard />
+  <q-page class="flex flex-center q-gutter-x-md">
+    <EdgeCard :imageUrl = menuUrl label="Menu" color="var(--nord4)" />
+    <EdgeCard label="Plats" :imageUrl=platUrl color="var(--nord12)" />
+    <EdgeCard label="Ingredients" :imageUrl=ingredientUrl color="var(--nord13)" />
+
+
   </q-page>
 </template>
 
