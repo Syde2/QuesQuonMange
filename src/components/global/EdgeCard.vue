ed6e86002a14944015e3bd1985a5c10c95f2db71
Syde2 <contact@syde2hub.com>
2024-04-06 09:28:20 +0200

    commit de la veille

--- src/components/global/EdgeCard.vue
+++ src/components/global/EdgeCard.vue
@@ -1,71 +1,115 @@
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
+import { gsap, timeline } from 'boot/gsap'
 
 const props = defineProps({
-  imageUrl : {
-    type:String,
-    default:'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=2810&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  imageUrl: {
+    type: String,
+    default: 'https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=2810&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   },
-  label : {
-    type:String,
-    default:'Label'
+  label: {
+    type: String,
+    default: 'Label'
   },
-  color :String
+  color: String
 })
-
 const image = `url(${props.imageUrl})`
+const menu = ref(false)
+const element = ref()
+
+
+
+function onEnter(el, done) {
+  element.value =el
+   timeline.fromTo(el, {
+    y:10,
+    yoyo: true,
+    repeat:-1,
+    ease: "power1.inOut",
+    duration:1,
+  },
+  {
+    y:-10,
+    yoyo: true,
+    repeat:-1,
+    ease: "power1.inOut",
+    duration:1,
+    onComplete: done,
+  }
+
+  )
+}
+
+function handleHover() {
+ menu.value = true
+ timeline.resume()
+}
+
+function handleMouseOut(){
+  timeline.pause()
+  menu.value = false
+}
+
+function handleClick(){
+  timeline.to(element, {
+    scaleX:1, width:100
+  })
+
+}
+
 
 </script>
 
-
 <template>
+  <Transition @enter="onEnter" :css="false">
+    <div :key="menu"  @mouseover="handleHover" @mouseleave="handleMouseOut" @click="handleClick" class="box_parent ">
+      <div class="box2 reverse">
+        <h2> {{ props.label }} </h2>
+      </div>
+      <div class="box2 background">
 
-<div class="box_parent ">
-    <div class="box2 reverse">
-      <h2> {{props.label}} </h2>
-    </div>
-    <div class="box2 background">
-
-    </div>
-    <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
+      </div>
+      <svg class="flt_svg" xmlns="http://www.w3.org/2000/svg">
         <defs>
-            <filter id="flt_tag">
-                <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
-                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 29 -8" result="flt_tag" />
-                <feComposite in="SourceGraphic" in2="flt_tag" operator="atop"/>
-            </filter>
+          <filter id="flt_tag">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 29 -8"
+              result="flt_tag" />
+            <feComposite in="SourceGraphic" in2="flt_tag" operator="atop" />
+          </filter>
         </defs>
-    </svg>
-</div>
-
-
-
+      </svg>
+    </div>
+  </Transition>
 </template>
 
 <style lang="scss">
-.box2{
-    width:20rem;
-    background-color: v-bind(color);
-    height: 229px;
-    clip-path: polygon(100% 100%,0% 100%,0% 62px,100% 0%);
-    margin: 0 auto;
-    overflow: scroll;
-    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    font-size: 3rem;
+.box2 {
+  width: 20rem;
+  background-color: v-bind(color);
+  height: 229px;
+  clip-path: polygon(100% 100%, 0% 100%, 0% 62px, 100% 0%);
+  margin: 0 auto;
+  overflow: scroll;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 3rem;
 }
-.reverse{
+
+.reverse {
   rotate: 180deg;
 
 }
-.reverse h2{
+
+.reverse h2 {
   transform: scale(-1, -1);
 
 }
-.background{
+
+.background {
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
@@ -73,18 +117,15 @@ const image = `url(${props.imageUrl})`
 }
 
 .flt_svg {
-    visibility: hidden;
-    position: absolute;
-    width: 0px;
-    height: 0px;
+  visibility: hidden;
+  position: absolute;
+  width: 0px;
+  height: 0px;
 }
 
-.box_parent
-{
-    filter: url('#flt_tag');
-    cursor: pointer;
+.box_parent {
+  filter: url('#flt_tag');
+  cursor: pointer;
 
 }
-
-
 </style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,50 +1,23 @@
 <script setup>
 import EdgeCard from 'src/components/global/EdgeCard.vue'
-import { gsap, timeline } from 'boot/gsap'
-import {ref} from 'vue'
 
 const menuUrl = 'https://images.unsplash.com/photo-1524237492620-14fcdd03ce16?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 const platUrl = 'https://plus.unsplash.com/premium_photo-1675676486598-733979a2218f?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 const ingredientUrl = 'https://plus.unsplash.com/premium_photo-1677619680770-4feb47c4ebcc?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
 
-const menu =ref()
-
-function onBeforeEnter(el) {
-  timeline.delay(0.5)
-}
-function onEnter(el, done) {
-  timeline.from(el, {
-    x: 100, y: 100, scale: 0.5, rotation: 180, skewX: 45,
-
-
-    onComplete: done,
-  })
-
-}
-function handleClick(e){
-  timeline.from(menu.value, {
-    x: 100, y: 100, scale: 0.5, rotation: 180, skewX: 45,
-
-
-  })
-
-
-}
 
 
 </script>
 
 <template>
   <q-page class="flex flex-center q-gutter-x-md">
-    <Transition appear @before-enter="onBeforeEnter" @enter="onEnter" :css="false">
-      <div class="flex fit justify-evenly">
-        <EdgeCard ref="menu" @click="handleClick" class="" :imageUrl=menuUrl label="Menu" color="var(--nord4)" />
-        <EdgeCard class="" label="Ingredients" :imageUrl=ingredientUrl color="var(--nord13)" />
-        <EdgeCard class="" label="Plats" :imageUrl=platUrl color="var(--nord12)" />
-      </div>
+    <div class="flex fit justify-evenly" style=" transform: scale(0.7)">
+      <EdgeCard  :imageUrl=menuUrl label="Menu" color="var(--nord4)" />
+      <EdgeCard class="" label="Ingredients" :imageUrl=ingredientUrl color="var(--nord13)" />
+      <EdgeCard class="" label="Plats" :imageUrl=platUrl color="var(--nord12)" />
+    </div>
 
 
-    </Transition>
 
   </q-page>
 </template>
