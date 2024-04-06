e5f4d771134876062c9b3031ac6a09553d399502
Syde2 <contact@syde2hub.com>
2024-04-06 19:03:59 +0200

    images

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,10 @@
 <script setup>
 import EdgeCard from 'src/components/global/EdgeCard.vue'
 
-const menuUrl = 'https://images.unsplash.com/photo-1524237492620-14fcdd03ce16?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-const platUrl = 'https://plus.unsplash.com/premium_photo-1675676486598-733979a2218f?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-const ingredientUrl = 'https://plus.unsplash.com/premium_photo-1677619680770-4feb47c4ebcc?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+
+const menuUrl = './menus.jpg'
+const platUrl ='plat.jpg'
+const ingredientUrl = '/ingredients.jpg'
 
 
 
