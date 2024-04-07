81a469d1e91ace81f7f9a496541dc425cb6ea70d
Syde2 <contact@syde2hub.com>
2024-04-07 10:59:49 +0200

    refactor

---
+++ src/components/global/EdgeCardGrid.vue
@@ -0,0 +1,12 @@
+<script setup>
+import EdgeCard from './EdgeCard.vue';
+</script>
+
+<template>
+  <div class="flex fit justify-evenly" style=" transform: scale(0.7)">
+    <EdgeCard path="/menu" imageUrl="menus.jpg" label="Menu" color="var(--nord4)" />
+    <EdgeCard path="/ingredients" imageUrl="ingredients.jpg" label="Ingredients" color="var(--nord13)" />
+    <EdgeCard path="/plats" imageUrl='plat.jpg' label="Plats" color="var(--nord12)" />
+  </div>
+
+</template>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,14 +1,9 @@
 <script setup>
-import EdgeCard from 'src/components/global/EdgeCard.vue'
+import EdgeCardGrid from 'src/components/global/EdgeCardGrid.vue'
 </script>
 
 <template>
   <q-page class="flex flex-center q-gutter-x-md">
-    <div class="flex fit justify-evenly" style=" transform: scale(0.7)">
-      <EdgeCard path="/menu" imageUrl= "menus.jpg" label="Menu" color="var(--nord4)"  />
-      <EdgeCard path="/ingredients" imageUrl="ingredients.jpg" label="Ingredients" color="var(--nord13)" />
-      <EdgeCard path="/plats" imageUrl='plat.jpg'  label="Plats"  color="var(--nord12)" />
-    </div>
-
+    <EdgeCardGrid />
   </q-page>
 </template>
